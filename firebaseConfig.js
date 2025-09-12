c07f1059915cdb9298e679efb697aa43bb178ddb
Sofía González <sofiagonzalez3712@gmail.com>
2025-09-12 00:13:17 -0600

    65

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARV4ym-z0i2JEox5yZVG0c0LPxSe7FFj4",
-  authDomain: "bodabetoyale.firebaseapp.com",
-  databaseURL: "https://bodabetoyale-default-rtdb.firebaseio.com",
-  projectId: "bodabetoyale",
-  storageBucket: "bodabetoyale.appspot.com",
-  messagingSenderId: "569441504988",
-  appId: "1:569441504988:web:7fc6c22c3a3943a253ae19"
+  apiKey: "AIzaSyBOwbnoAsLw_lW9OPOsumJELAN1JIDbfMk",
+  authDomain: "boda-meli-y-joel.firebaseapp.com",
+  projectId: "boda-meli-y-joel",
+  storageBucket: "boda-meli-y-joel.firebasestorage.app",
+  messagingSenderId: "89836674580",
+  appId: "1:89836674580:web:991509c3f39f8725a3d101",
+  measurementId: "G-DZY1TCXPNM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
